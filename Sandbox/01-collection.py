83c0da79d27a5a90b798b8ad33e038248a58170a
Krzysztof Krzywdzinski <krzysztof.krzywdzinski@intel.com>
2022-06-10 20:13:13 +0200

    Commit to move to Blaszak

---
+++ Sandbox/01-collection.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def fub():
+    print("bar")
+
+
+country_list: list[str] = ["poland", "germany"]
+country_tuple: tuple[str] = ("poland", "germany")
+country_set: set[str] = {"poland", "Germany"}
+country_dict = {"poland": "warszawa", "Niemcy": "Berlin"}
+
+
+
+fub()
---
+++ Sandbox/02-clock.py
@@ -0,0 +1,6 @@
+#this will be Clock progam to simulate gears
+
+def print_name_of_gear(name: object) -> object:
+    print("name")
+
+print_name_of_gear('1')
