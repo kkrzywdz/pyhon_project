321e87fc4fedc05e1ef2fa463baa5cfeeecd5fc2
Krzywdzinski, Krzysztof <krzysztof.krzywdzinski@intel.com>
2023-01-11 09:29:09 +0100

    Battleship2: draw board in screen
     Changes to be committed:
    	modified:   Sandbox/Battleship2.py

--- Sandbox/Battleship2.py
+++ Sandbox/Battleship2.py
@@ -7,8 +7,14 @@ pygame.init()
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 
-# Load the image
-image = pygame.image.load("./Data/T_Battleship.png")
+# Set the board size
+board_size = (10, 10)
+
+# Set the square size
+square_size = (20, 20)
+
+# Set the board position
+board_pos = (100, 100)
 
 # Load the images
 images = []
@@ -19,6 +25,11 @@ images.append(pygame.image.load("./Data/T_Cruiser.png"))
 images.append(pygame.image.load("./Data/T_Destroyer.png"))
 images.append(pygame.image.load("./Data/T_GunBoat.png"))
 images.append(pygame.image.load("./Data/T_Submarine.png"))
+
+# mirrored images
+mirrorImages = []
+for image in images:
+    mirrorImages.append(pygame.transform.flip(image, True, False))
 
 # Main loop
 running = True
@@ -33,6 +44,14 @@ while running:
     for image in images:
         screen.blit(image, (x, y))
         y = y + 100
+
+    # Draw the board
+    for i in range(board_size[0]):
+        for j in range(board_size[1]):
+            pygame.draw.rect(screen, (255, 255, 255), (board_pos[0] + i * square_size[0],
+                                                       board_pos[1] + j * square_size[1],
+                                                       square_size[0], square_size[1]), 0)
+            pygame.Surface.fill((i, j) * square_size, (255, 0, 0))
 
     # Update the display
     pygame.display.flip()
