23188df74340d300104086874c1d47804daf3193
Cube - Krzysiek <ashram@interia.pl>
2023-01-14 18:59:04 +0100

    Battleship2.py - Added mirrored ships, and scale, position in the board

--- Sandbox/Battleship2.py
+++ Sandbox/Battleship2.py
@@ -9,13 +9,13 @@ window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 
 # Set the board size
-board_size = (10, 10)
+board_size = (20, 20)
 
 # Set the square size
 square_size = (20, 20)
 
 # Set the board position
-board_pos = (100, 100)
+board_pos = (200, 0)
 
 # define bord colors
 board_color_enpty = 100, 100, 200
@@ -25,14 +25,18 @@ board_color_destroied = 63, 63, 63
 
 
 # Load the images
+RAWimages = []
+RAWimages.append(pygame.image.load("./Data/T_Carrier.png"))
+RAWimages.append(pygame.image.load("./Data/T_Battleship.png"))
+RAWimages.append(pygame.image.load("./Data/T_Corvette.png"))
+RAWimages.append(pygame.image.load("./Data/T_Cruiser.png"))
+RAWimages.append(pygame.image.load("./Data/T_Destroyer.png"))
+RAWimages.append(pygame.image.load("./Data/T_GunBoat.png"))
+RAWimages.append(pygame.image.load("./Data/T_Submarine.png"))
+
 images = []
-images.append(pygame.image.load("./Data/T_Carrier.png"))
-images.append(pygame.image.load("./Data/T_Battleship.png"))
-images.append(pygame.image.load("./Data/T_Corvette.png"))
-images.append(pygame.image.load("./Data/T_Cruiser.png"))
-images.append(pygame.image.load("./Data/T_Destroyer.png"))
-images.append(pygame.image.load("./Data/T_GunBoat.png"))
-images.append(pygame.image.load("./Data/T_Submarine.png"))
+for image in RAWimages:
+    images.append(pygame.transform.scale (image, (200,50)))
 
 # mirrored images
 mirrorImages = []
@@ -46,12 +50,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill((255, 255, 255))
+
     # Blit the image to the screen
     x = 0
     y = 0
     for image in images:
         screen.blit(image, (x, y))
-        y = y + 100
+        y = y + 60
+
+    x = 600
+    y = 0
+    for image in mirrorImages:
+        screen.blit(image, (x, y))
+        y = y + 60
 
     # Draw the board
     for i in range(board_size[0]):
@@ -62,7 +74,7 @@ while running:
             pygame.draw.rect(screen, board_color, (board_pos[0] + i * square_size[0],
                                                        board_pos[1] + j * square_size[1],
                                                        square_size[0] - 1, square_size[1] - 1), 0)
-            #pygame.Surface.fill((255, 0, 0))
+
 
 
     # Update the display
